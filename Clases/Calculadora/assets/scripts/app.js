477d8914d834c702a6649a14a1a7fed48e645a89
sergio <sergiohidalgocaceres@gmail.com>
2021-03-20 18:51:55 -0500

    primera función

--- Clases/Calculadora/assets/scripts/app.js
+++ Clases/Calculadora/assets/scripts/app.js
@@ -1,16 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-
-// currentResult = currentResult + 10;
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-// let calculationDescription = '((' + defaultResult + ' + 10) * 3) / 2 - 1';
-// let calculationDescription = `((${defaultResult} + 10) * 3) / 2 - 1`;
-let calculationDescription = `(
-  (${defaultResult} + 10) * 3) 
-  / 2 
-  - 1`;
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert('Result ' + result);
+}
 
-// let errorMessage = 'An error \\ ocurred!';
-let errorMessage = "An error ' ocurred!";
-outputResult(currentResult, errorMessage);
+add(10, 20);
+add(5, 3);
+
+let calculationDescription = `((${defaultResult} + 10) * 3) / 2 - 1`;
+outputResult(currentResult, calculationDescription);
