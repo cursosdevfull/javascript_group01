34c4beef2bc652ea4cdeab5eafc4bb8e79ab3f7d
sergio <sergiohidalgocaceres@gmail.com>
2021-03-20 18:55:48 -0500

    retornar valor

--- Clases/Calculadora/assets/scripts/app.js
+++ Clases/Calculadora/assets/scripts/app.js
@@ -4,11 +4,12 @@ currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 function add(num1, num2) {
   const result = num1 + num2;
-  alert('Result ' + result);
+  return result;
 }
 
-add(10, 20);
-add(5, 3);
+/* const additionalResult = add(10, 20);
+currentResult = additionalResult; */
+currentResult = add(10, 20);
 
 let calculationDescription = `((${defaultResult} + 10) * 3) / 2 - 1`;
 outputResult(currentResult, calculationDescription);
