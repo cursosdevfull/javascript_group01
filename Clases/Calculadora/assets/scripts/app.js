d1760b5269a7a9fa62850ff51f048a2ce77fec89
sergio <sergiohidalgocaceres@gmail.com>
2021-03-20 18:46:23 -0500

    caracter de escape y backtick

--- Clases/Calculadora/assets/scripts/app.js
+++ Clases/Calculadora/assets/scripts/app.js
@@ -4,5 +4,13 @@ let currentResult = defaultResult;
 // currentResult = currentResult + 10;
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = '((' + defaultResult + ' + 10) * 3) / 2 - 1';
-outputResult(currentResult, calculationDescription);
+// let calculationDescription = '((' + defaultResult + ' + 10) * 3) / 2 - 1';
+// let calculationDescription = `((${defaultResult} + 10) * 3) / 2 - 1`;
+let calculationDescription = `(
+  (${defaultResult} + 10) * 3) 
+  / 2 
+  - 1`;
+
+// let errorMessage = 'An error \\ ocurred!';
+let errorMessage = "An error ' ocurred!";
+outputResult(currentResult, errorMessage);
