d6191ae7064bde689d882a519bcd48aebe25d019
sergio <sergiohidalgocaceres@gmail.com>
2021-03-20 18:59:32 -0500

    ambito

--- Clases/Calculadora/assets/scripts/app.js
+++ Clases/Calculadora/assets/scripts/app.js
@@ -5,11 +5,14 @@ currentResult = ((currentResult + 10) * 3) / 2 - 1;
 function add(num1, num2) {
   const result = num1 + num2;
   return result;
+
+  alert('hola a todos');
 }
 
 /* const additionalResult = add(10, 20);
 currentResult = additionalResult; */
 currentResult = add(10, 20);
+alert('value ' + result);
 
 let calculationDescription = `((${defaultResult} + 10) * 3) / 2 - 1`;
 outputResult(currentResult, calculationDescription);
