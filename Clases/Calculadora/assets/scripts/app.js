999fb410c1cdc4e944c9b546dc7b174259674113
sergio <sergiohidalgocaceres@gmail.com>
2021-03-20 19:23:47 -0500

    conversion de datos

--- Clases/Calculadora/assets/scripts/app.js
+++ Clases/Calculadora/assets/scripts/app.js
@@ -2,7 +2,11 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + userInput.value;
+  //currentResult = currentResult + userInput.value;
+  currentResult = currentResult + parseInt(userInput.value);
+  // currentResult = currentResult + parseFloat(userInput.value);
+  //currentResult = currentResult + +userInput.value;
+  // currentResult = currentResult.toString() + parseInt(userInput.value);
   outputResult(currentResult, '');
 }
 
