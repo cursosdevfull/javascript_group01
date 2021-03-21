734719f9311add276ef636ad0cb5f4e4f6aa9038
sergio <sergiohidalgocaceres@gmail.com>
2021-03-20 19:12:43 -0500

    ejecución indirecta de una función

--- Clases/Calculadora/assets/scripts/app.js
+++ Clases/Calculadora/assets/scripts/app.js
@@ -1,18 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
-
-  alert('hola a todos');
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
 }
 
-/* const additionalResult = add(10, 20);
-currentResult = additionalResult; */
-currentResult = add(10, 20);
-alert('value ' + result);
-
-let calculationDescription = `((${defaultResult} + 10) * 3) / 2 - 1`;
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
