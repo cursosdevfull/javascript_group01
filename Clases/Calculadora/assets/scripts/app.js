87b3010818774ec8e7e869bdb3b187c51ac51708
sergio <sergiohidalgocaceres@gmail.com>
2021-03-20 19:29:54 -0500

    dividiendo funciones

--- Clases/Calculadora/assets/scripts/app.js
+++ Clases/Calculadora/assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
-  //currentResult = currentResult + userInput.value;
-  currentResult = currentResult + parseInt(userInput.value);
-  // currentResult = currentResult + parseFloat(userInput.value);
-  //currentResult = currentResult + +userInput.value;
-  // currentResult = currentResult.toString() + parseInt(userInput.value);
-  outputResult(currentResult, '');
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
