f9971c865d9a13c7b09eb9180332dae38b3e9ae9
sergio <sergiohidalgocaceres@gmail.com>
2021-03-27 17:56:16 -0500

    ganador-perdedor

--- Clases/Moustro/assets/scripts/app.js
+++ Clases/Moustro/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 14;
+const MONSTER_ATTACK_VALUE = 15;
 
 let choseMaxLife = 100;
 let currentMonsterHealth = choseMaxLife;
@@ -9,6 +10,16 @@ adjustHealthBars(choseMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayHealth > 0) {
+    alert('Ganó');
+  } else if (currentPlayHealth <= 0 && currentMonsterHealth > 0) {
+    alert('Perdió');
+  } else if (currentMonsterHealth <= 0 && currentPlayHealth <= 0) {
+    alert('Empate');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
