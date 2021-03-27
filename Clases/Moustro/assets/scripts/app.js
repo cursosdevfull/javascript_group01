19620098e73c1bc7e89e7e62cd3f3488fa07819e
sergio <sergiohidalgocaceres@gmail.com>
2021-03-27 18:04:24 -0500

    ataque fuerte

--- Clases/Moustro/assets/scripts/app.js
+++ Clases/Moustro/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 14;
 const MONSTER_ATTACK_VALUE = 15;
+const STRONG_ATTACK_VALUE = 17;
 
 let choseMaxLife = 100;
 let currentMonsterHealth = choseMaxLife;
@@ -7,8 +8,15 @@ let currentPlayHealth = choseMaxLife;
 
 adjustHealthBars(choseMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonsterHandler(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayHealth -= playerDamage;
@@ -22,4 +30,13 @@ function attackHandler() {
   }
 }
 
+function attackHandler() {
+  attackMonsterHandler('ATTACK');
+}
+
+function strongAttackHandler() {
+  attackMonsterHandler('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
