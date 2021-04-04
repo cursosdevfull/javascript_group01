77249cdc7f5140724c708a6064fa84f7e2edccd0
sergio <sergiohidalgocaceres@gmail.com>
2021-04-03 19:24:10 -0500

    implementando juego

--- Clases/Piedra-Papel-Tijera/assets/scripts/app.js
+++ Clases/Piedra-Papel-Tijera/assets/scripts/app.js
@@ -1,11 +1,35 @@
 'use strict';
 
-const startGame = function () {
-  console.log('Game is starting');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSOR = 'SCISSOR';
+const DEFAULT_USER_CHOICE = ROCK;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `Elija ${ROCK}, ${PAPER} o ${SCISSOR}`
+  ).toUpperCase();
+
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSOR) {
+    alert(
+      `La selección es solo ${ROCK}, ${PAPER} o ${SCISSOR}. Hemos escogido ${DEFAULT_USER_CHOICE} por usted`
+    );
+
+    return DEFAULT_USER_CHOICE;
+  }
+
+  return selection;
 };
 
-console.dir(startGame);
+let gameIsRunning = false;
 
 startBtn.addEventListener('click', function () {
-  console.log('Game is starting...');
+  if (gameIsRunning) {
+    return;
+  }
+
+  gameIsRunning = true;
+  console.log('Game is starting');
+  const playerChoice = getPlayerChoice();
+  console.log(playerChoice);
 });
