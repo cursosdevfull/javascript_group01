8be1d79165962581b26e2e98817276da0a884b50
sergio <sergiohidalgocaceres@gmail.com>
2021-04-03 19:07:02 -0500

    funciones expresiones

--- Clases/Piedra-Papel-Tijera/assets/scripts/app.js
+++ Clases/Piedra-Papel-Tijera/assets/scripts/app.js
@@ -1,18 +1,7 @@
 'use strict';
 
-function startGame() {
-  console.log('Game is starting...');
-}
-
-const game = {
-  title: 'Juego Piedra, Papel y Tijera',
-  start: function start() {
-    console.log('Game is starting...');
-  },
+const startGame = function start() {
+  console.log('Game is starting');
 };
 
-console.log(typeof startGame);
-console.dir(startGame);
-
-startBtn.addEventListener('click', game.start);
-// startGame();
+startBtn.addEventListener('click', startGame);
