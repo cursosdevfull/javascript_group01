7acf378860b9d5b8b50266165927c529da005473
sergio <sergiohidalgocaceres@gmail.com>
2021-05-02 07:24:51 -0500

    mini-tienda

--- Clases/MiniTienda/assets/scripts/app.js
+++ Clases/MiniTienda/assets/scripts/app.js
@@ -1,9 +1,4 @@
 class Product {
-  /*   title;
-  imageUrl;
-  description;
-  price; */
-
   constructor(title, imageUrl, description, price) {
     this.title = title;
     this.imageUrl = imageUrl;
@@ -12,19 +7,89 @@ class Product {
   }
 }
 
-class ProductItem {
-  constructor(product) {
-    this.product = product;
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+class Component {
+  constructor(renderHookId, shouldRenderer = true) {
+    this.hookId = renderHookId;
+    if (shouldRenderer) {
+      this.render();
+    }
   }
 
-  addToCart() {
-    console.log('adding product to cart');
-    console.log(this.product);
+  render() {}
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
+  items = [];
+
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
+
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `Total: S/.${this.totalAmount.toFixed(2)}`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+
+    return sum;
+  }
+
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
   }
 
   render() {
-    const prodEl = document.createElement('li');
-    prodEl.className = 'product-item';
+    const cartEl = this.createRootElement('section', 'cart');
+    cartEl.innerHTML = `
+      <h2>Total: 0</h2>
+      <button>Order Now</button>
+    `;
+    this.totalOutput = cartEl.querySelector('h2');
+    return cartEl;
+  }
+}
+
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
+    this.product = product;
+    this.render();
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodEl = this.createRootElement('li', 'product-item');
     prodEl.innerHTML = `
     <div>
       <img src="${this.product.imageUrl}" alt="${this.product.title}">
@@ -40,73 +105,73 @@ class ProductItem {
     `;
     const addCartButton = prodEl.querySelector('button');
     addCartButton.addEventListener('click', this.addToCart.bind(this));
-    return prodEl;
   }
 }
 
-class ProductList {
-  products = [
-    new Product(
-      'Almohada',
-      'https://static8.depositphotos.com/1177973/1040/i/600/depositphotos_10404781-stock-photo-bright-pillows-isolated-on-white.jpg',
-      'Almohada de lujo',
-      200
-    ),
-    new Product(
-      'Sábanas',
-      'https://ep01.epimg.net/elpais/imagenes/2018/06/07/icon/1528362397_392956_1528372261_noticia_normal.jpg',
-      'Sábanas de seda egipcia',
-      650
-    ),
-  ];
+class ProductList extends Component {
+  products = [];
+
+  constructor(renderHookId) {
+    super(renderHookId);
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.products = [
+      new Product(
+        'Almohada',
+        'https://static8.depositphotos.com/1177973/1040/i/600/depositphotos_10404781-stock-photo-bright-pillows-isolated-on-white.jpg',
+        'Almohada de lujo',
+        200
+      ),
+      new Product(
+        'Sábanas',
+        'https://ep01.epimg.net/elpais/imagenes/2018/06/07/icon/1528362397_392956_1528372261_noticia_normal.jpg',
+        'Sábanas de seda egipcia',
+        650
+      ),
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod, 'prod-list');
+    }
+  }
 
   render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+    this.createRootElement('ul', 'product-list', [
+      new ElementAttribute('id', 'prod-list'),
+    ]);
+    if (this.products && this.products.length > 0) {
+      this.renderProducts();
     }
-    renderHook.append(prodList);
   }
 }
 
-const productList = new ProductList();
-productList.render();
-
-/* class Person {
-  name;
-  lastname;
-}
-
-
-
-const nameCustomer = document.getElementById('name');
-const lastnameCustomer = document.getElementById('lastname');
-const btnSend = document.getElementById('send');
-
-const listPersons = [];
-
-const addPerson = () => {
-  if (
-    nameCustomer.value.trim() === '' ||
-    lastnameCustomer.value.trim() === ''
-  ) {
-    return;
+class Shop {
+  constructor() {
+    this.render();
   }
 
-  const person = new Person();
-  person.name = nameCustomer.value;
-  person.lastname = lastnameCustomer.value;
+  render() {
+    this.cart = new ShoppingCart('app');
+    new ProductList('app');
+  }
+}
 
-  listPersons.push(person);
-  console.log(listPersons);
+class App {
+  static cart;
 
-  nameCustomer.value = '';
-  lastnameCustomer.value = '';
-};
+  static init() {
+    const shop = new Shop();
+    this.cart = shop.cart;
+  }
 
-btnSend.addEventListener('click', addPerson);
- */
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
